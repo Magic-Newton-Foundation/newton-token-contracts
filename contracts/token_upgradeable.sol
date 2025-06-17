fd35966a092775149954f66101c0d0680a4643d6
Josh Mac <josh.mac@magic.link>
2025-06-17 12:28:13 -0400

    Newton token contracts

---
+++ contracts/token_upgradeable.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {ERC20BurnableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract CustomTokenUpgradeable is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, AccessControlUpgradeable, ERC20PermitUpgradeable, UUPSUpgradeable {
+    uint256 private _cap;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(
+        string memory name,
+        string memory symbol,
+        uint256 cap_,
+        address defaultAdmin,
+        address[] memory minters
+    ) initializer public
+    {
+        require(cap_ > 0, "Cap must be greater than 0");
+        _cap = cap_;
+        __ERC20_init(name, symbol);
+        __ERC20Burnable_init();
+        __AccessControl_init();
+        __ERC20Permit_init(name);
+        __UUPSUpgradeable_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        for(uint i = 0; i < minters.length; i++) {
+            _grantRole(MINTER_ROLE, minters[i]);
+        }
+    }
+
+    function supplyCap() public view returns (uint256) {
+        return _cap;
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        require(totalSupply() + amount <= supplyCap(), "ERC20: cap exceeded");
+        _mint(to, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyRole(DEFAULT_ADMIN_ROLE)
+        override
+    {}
+}
